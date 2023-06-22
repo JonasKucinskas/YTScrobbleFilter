3c930a6a5cc746eb23e6ace8c027999904085fea
Jonas <jonas@jonas.lan>
2023-06-22 23:04:04 +0300

    huge code fixes and optimizations

--- app/src/main/java/com/example/ytscrobblefilter/MainActivity.kt
+++ app/src/main/java/com/example/ytscrobblefilter/MainActivity.kt
@@ -3,17 +3,18 @@ package com.example.ytscrobblefilter
 import android.Manifest
 import android.accounts.Account
 import android.content.Intent
-import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.content.pm.PackageManager.PERMISSION_DENIED
 import android.net.ConnectivityManager
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
 import com.google.android.gms.common.api.ApiException
 import com.google.api.client.extensions.android.http.AndroidHttp
@@ -25,11 +26,13 @@ import com.google.api.client.util.ExponentialBackOff
 import com.google.api.services.youtube.YouTube
 import com.google.api.services.youtube.YouTubeScopes
 import com.google.api.services.youtube.model.Channel
+import com.google.api.services.youtube.model.ChannelListResponse
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
+import java.lang.IllegalArgumentException
 
 class MainActivity : AppCompatActivity() {
 
@@ -39,6 +42,8 @@ class MainActivity : AppCompatActivity() {
     private var mService: YouTube? = null
     private val scope = CoroutineScope(Dispatchers.IO)
 
+    private val RESULT_OK = 0
+
     val REQUEST_ACCOUNT_PICKER = 1000
     val REQUEST_AUTHORIZATION = 1001
     val REQUEST_GOOGLE_PLAY_SERVICES = 1002
@@ -47,9 +52,67 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         mCredential = getCredential()
+        mServiceInnit()
 
+        checkPermissions()
 
+        if (mCredential.selectedAccountName == null) {
+            signIn()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        var data: List<String?>? = null
+
+        val button = findViewById<Button>(R.id.button)
+        val text = findViewById<TextView>(R.id.text)
+
+        button.setOnClickListener(){
+            //TODO sometimes this bugs, idk why, will fix later.
+            scope.launch {
+
+                val deferredData = async { dataFromApi() }
+                data = deferredData.await()
+
+            }
+            if (data != null){
+                text.text = data!![0]
+            }
+        }
+    }
+
+    //TODO userinformation isnt saved, so this is launched every time app is launched.
+    private val signInLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+
+        val data: Intent? = result.data
+        if (data != null) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Handle successful sign-in
+                val account = task.getResult(ApiException::class.java)
+                mCredential.selectedAccount = Account(account.email, "com.example.ytscrobblefilter")
+                // Use the account for further API requests or store the access token
+                Log.e("TAG", "Sign-in succeeded")
+            } catch (e: ApiException) {
+                // Handle sign-in failure
+                Log.e("TAG", "Sign-in failed", e)
+            }
+        }
+    }
+
+    private fun signIn() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        signInLauncher.launch(googleSignInClient.signInIntent)
+    }
+
+    private fun mServiceInnit(){
         val transport = AndroidHttp.newCompatibleTransport()
         val jsonFactory: JsonFactory = JacksonFactory.getDefaultInstance()
         mService = YouTube.Builder(
@@ -57,58 +120,14 @@ class MainActivity : AppCompatActivity() {
         )
             .setApplicationName("YTScrobbleFilter")
             .build()
-
-        // Call this method to start the sign-in process
-        signIn()
-        chooseAccount()
-        var data: List<String?>
-
-        scope.launch {
-            val deferredData = async { dataFromApi() }
-
-            try{
-                data = deferredData.await()
-                val test = 0
-            }
-            catch (e: UserRecoverableAuthIOException){
-                startActivityForResult(e.intent,REQUEST_AUTHORIZATION)
-            }
-            // Use the data or update UI with the fetched data
-        }
-
     }
-
-    private fun mServiceInnit(credential: GoogleAccountCredential){
-
-    }
-
-
-
-
-
     override fun onDestroy() {
         super.onDestroy()
         scope.cancel()
     }
 
-
     private fun getCredential(): GoogleAccountCredential {
         return GoogleAccountCredential.usingOAuth2(this, listOf(YouTubeScopes.YOUTUBE_READONLY)).setBackOff(ExponentialBackOff())
-    }
-
-    private fun getResultsFromApi(): List<String?>{
-        if (GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this) != ConnectionResult.SUCCESS) {
-            acquireGooglePlayServices()
-        }
-        if (mCredential.selectedAccountName == null) {
-            chooseAccount()
-        }
-        if (!isDeviceOnline()) {
-            Toast.makeText(this, "No network connection available.", Toast.LENGTH_SHORT).show()
-        }
-
-
-        return dataFromApi()
     }
 
     private fun acquireGooglePlayServices() {
@@ -134,68 +153,26 @@ class MainActivity : AppCompatActivity() {
         return networkInfo != null && networkInfo.isConnected
     }
 
-
-    private fun signIn() {
-
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestEmail()
-            .build()
-        googleSignInClient = GoogleSignIn.getClient(this, gso)
-
-        startActivityForResult(googleSignInClient.signInIntent, RC_SIGN_IN)
-    }
-
-    private fun chooseAccount() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.GET_ACCOUNTS) == PERMISSION_GRANTED) {
-            val accountName = "Jonas"
-
-            if (accountName != null) {
-
-                mCredential.selectedAccount = Account("jonaskucinskas2@gmail.com", "com.example.ytscrobblefilter")
-            } else {
-                // Start a dialog from which the user can choose an account
-                startActivityForResult(
-                    mCredential.newChooseAccountIntent(),
-                    1000
-                )
-            }
-        } else {
-            // Request the GET_ACCOUNTS permission via a user dialog
-            requestPermissions(arrayOf(Manifest.permission.GET_ACCOUNTS),1003)
-
-        }
-    }
-
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == RC_SIGN_IN) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            try {
-                // Handle successful sign-in
-                val account = task.getResult(ApiException::class.java)
-                // Use the account for further API requests or store the access token
-                val accessToken = account?.idToken
-                Log.e("TAG", "Sign-in succeeded")
-            } catch (e: ApiException) {
-                // Handle sign-in failure
-                Log.e("TAG", "Sign-in failed", e)
-            }
-        }
-
-        if (requestCode == REQUEST_AUTHORIZATION) {
-            signIn()
+    private fun checkPermissions(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.GET_ACCOUNTS) == PERMISSION_DENIED) {
+            requestPermissions(arrayOf(Manifest.permission.GET_ACCOUNTS), 1003)
         }
     }
 
     private fun dataFromApi(): List<String?>{
         // Get a list of up to 10 files.
         val channelInfo: MutableList<String?> = ArrayList()
-        val result = mService!!.channels().list("snippet,contentDetails,statistics")
-            .setForUsername("GoogleDevelopers")
-            .execute()
-        val channels: List<Channel>? = result.items
+        var result: ChannelListResponse? = null
+        try{
+             result = mService!!.channels().list("snippet,contentDetails,statistics")
+                .setForUsername("GoogleDevelopers")
+                .execute()
+        }
+        catch (e: Exception){
+            Log.e("API", "API error while fetching data.")
+        }
+
+        val channels: List<Channel>? = result?.items
         if (channels != null) {
             val channel: Channel = channels[0]
             channelInfo.add(
